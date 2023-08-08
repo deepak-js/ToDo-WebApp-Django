f9f8be0d9db4ab05c5fbb4168cbcd32c82f38e27
Deepak Raja S <deepakselvam2000@gmail.com>
2023-08-08 16:32:54 +0530

    task function updated

--- todo_main/views.py
+++ todo_main/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from todo.models import Task
 
 
 def home(request):
-    return render(request, "home.html")
+    tasks = Task.objects.filter(is_completed=False).order_by('-updated_at')
+    context = {
+        'tasks': tasks,
+    }
+    return render(request, "home.html", context)
+    
